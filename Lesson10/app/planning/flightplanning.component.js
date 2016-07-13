6aa5448fb01d255f2067e8b3f72d10cf52bddf55
tom-schier <tomasschier@gmail.com>
2016-07-13 21:25:50 +1000

    Stub functions for calculated tracks

--- Lesson10/app/planning/flightplanning.component.js
+++ Lesson10/app/planning/flightplanning.component.js
@@ -1,4 +1,4 @@
-System.register(['./wind.component', '../services/weather.service', './track.component', 'angular2/core', '../services/aircraft.service', 'angular2/router'], function(exports_1, context_1) {
+System.register(['../services/track.service', './wind.component', '../services/weather.service', './track.component', 'angular2/core', '../services/aircraft.service', 'angular2/router'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,10 +10,13 @@ System.register(['./wind.component', '../services/weather.service', './track.com
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var wind_component_1, weather_service_1, track_component_1, core_1, aircraft_service_1, router_1;
+    var track_service_1, wind_component_1, weather_service_1, track_component_1, core_1, aircraft_service_1, router_1;
     var FlightPlanningComponent;
     return {
         setters:[
+            function (track_service_1_1) {
+                track_service_1 = track_service_1_1;
+            },
             function (wind_component_1_1) {
                 wind_component_1 = wind_component_1_1;
             },
@@ -34,10 +37,11 @@ System.register(['./wind.component', '../services/weather.service', './track.com
             }],
         execute: function() {
             FlightPlanningComponent = (function () {
-                function FlightPlanningComponent(_routeParams, _acService, _weatherService) {
+                function FlightPlanningComponent(_routeParams, _acService, _weatherService, _trackService) {
                     this._routeParams = _routeParams;
                     this._acService = _acService;
                     this._weatherService = _weatherService;
+                    this._trackService = _trackService;
                     console.log('Creating Flight Planning Component');
                 }
                 FlightPlanningComponent.prototype.ngOnInit = function () {
@@ -47,16 +51,31 @@ System.register(['./wind.component', '../services/weather.service', './track.com
                     this._acService.aircraftDetailsChange$.subscribe(function (acDetails) {
                         _this.UpdateAircraft(acDetails);
                     });
+                    this._weatherService.windDetailsChange$.subscribe(function (wnd) {
+                        _this.CalculateWindEffect(wnd);
+                    });
+                    this._trackService.trackDetailsChange$.subscribe(function (tr) {
+                        _this.CalculateTrackChanges(tr);
+                    });
                 };
                 FlightPlanningComponent.prototype.UpdateAircraft = function (theAircraft) {
                     this.selectedAircraft = theAircraft;
+                };
+                FlightPlanningComponent.prototype.CalculateWindEffect = function (winds) {
+                    // update the calcTrack array
+                };
+                FlightPlanningComponent.prototype.CalculateTrackChanges = function (tracks) {
+                    // empty the calcTrack array, recreate and take winds into account
+                };
+                FlightPlanningComponent.prototype.findClosestWind = function (aTrack) {
+                    // return a WindDetails object
                 };
                 FlightPlanningComponent = __decorate([
                     core_1.Component({
                         templateUrl: './flightplan.html',
                         directives: [wind_component_1.WindData, track_component_1.TrackData]
                     }), 
-                    __metadata('design:paramtypes', [router_1.RouteParams, aircraft_service_1.AircraftService, weather_service_1.WeatherService])
+                    __metadata('design:paramtypes', [router_1.RouteParams, aircraft_service_1.AircraftService, weather_service_1.WeatherService, track_service_1.TrackService])
                 ], FlightPlanningComponent);
                 return FlightPlanningComponent;
             }());
--- Lesson10/app/planning/flightplanning.component.ts
+++ Lesson10/app/planning/flightplanning.component.ts
@@ -1,16 +1,9 @@
 
-//import {Component, OnInit, Input, Output, EventEmitter,  Inject}   from 'angular2/core';
-import { TrackService}   from '../services/track.service';
-//import {RouteParams} from 'angular2/router';
-//import {AircraftService}  from '../aircraft/aircraft.service';
-//import {Aircraft}  from '../data/aircraft.types';
+import {TrackService, TrackComponent}   from '../services/track.service';
 import {WindData}  from './wind.component';
-import {WeatherService}  from '../services/weather.service';
+import {WeatherService, WindDetails}  from '../services/weather.service';
 import {TrackData}  from './track.component';
-
 import {Component, OnInit, Input, Output, EventEmitter, ViewEncapsulation, Inject}  from 'angular2/core';
-//import {AircraftSpeedsComponent} from './aircraft-speeds.component';
-//import {AircraftWeightsComponent} from './aircraft-weights.component';
 import {AircraftService}  from '../services/aircraft.service';
 import {RouteParams, Router} from 'angular2/router';
 import {Aircraft, AircraftBrief} from '../data/aircraft.types';
@@ -26,9 +19,10 @@ export class FlightPlanningComponent implements OnInit {
     selectedAircraft: Aircraft;
     windDirection: number;
     windSpeed: number;
+    calcTrack: TrackComponent[];
 
     constructor(private _routeParams: RouteParams, private _acService: AircraftService,
-                public _weatherService: WeatherService) {
+                public _weatherService: WeatherService, public _trackService: TrackService) {
         console.log('Creating Flight Planning Component');
     }
 
@@ -39,17 +33,32 @@ export class FlightPlanningComponent implements OnInit {
             acDetails => {
                 this.UpdateAircraft(acDetails);
             });
+
+        this._weatherService.windDetailsChange$.subscribe(
+            wnd => {
+                this.CalculateWindEffect(wnd);
+            });
+
+        this._trackService.trackDetailsChange$.subscribe(
+            tr => {
+                this.CalculateTrackChanges(tr);
+            });
     }
 
     UpdateAircraft(theAircraft: Aircraft) {
         this.selectedAircraft = theAircraft;
     }
 
-    //onSubmit() {
-    //    this.windDirection
-    //}
+    CalculateWindEffect(winds: WindDetails[]) {
+        // update the calcTrack array
+    }
 
-    //onclick(evt) {
+    CalculateTrackChanges(tracks: TrackComponent[]) {
+        // empty the calcTrack array, recreate and take winds into account
+    }
 
-    //}
+    findClosestWind(aTrack: TrackComponent) {
+        // return a WindDetails object
+    }
+
 }
